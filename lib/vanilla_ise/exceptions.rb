74c3cbe8bfa1fc2af5396a5cef7666f92331ca01
Shane Short <shanes@webinabox.net.au>
2023-07-04 17:36:34 +0800

    add new exception classes to better capture specific errors as required

--- lib/vanilla_ise/exceptions.rb
+++ lib/vanilla_ise/exceptions.rb
@@ -2,6 +2,24 @@
 
 module VanillaIse
   class Error < StandardError; end
+
   class CSRFRequired < Error; end
+
   class CSRFTokenExpired < Error; end
+
+  class NotFound < Error; end
+
+  class InvalidRequest < Error; end
+
+  class InvalidResponse < Error
+    attr_reader :parsed_response
+
+    def initialize(response, message: "An unknown error was encountered when submitting the data to Cisco ISE. Please try again.")
+      @parsed_response = response&.parsed_response
+      super(message)
+    end
+
+  end
+
+end
 end
